53941d2080736b14d8b7e5ccb2a8433a06a57b16
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-06 15:43:37 +0500

    add some documentation for fastapi and add new router

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from app.auth.base_config import router as auth_router
 from app.auth.database import create_db_and_tables
+from app.api import router
 
 
 @asynccontextmanager
@@ -11,7 +12,13 @@ async def lifespan(main_app: FastAPI):
     await create_db_and_tables()
     yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="Personal Webpage of Batirniyaz",
+    version="0.1",
+    summary="This is the personal webpage of Batirniyaz, where I will post my activity.",
+    lifespan=lifespan,
+)
 
 app.include_router(auth_router)
+app.include_router(router)
 
