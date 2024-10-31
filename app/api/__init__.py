32c9528e8fdf7940d6e5e4856cff27304ed79f13
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-31 13:16:40 +0500

    include talk and channel router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from .personal import router as personal_router
+from .talk import router as talk_router
+from .channel import router as channel_router
 
 router = APIRouter()
 
 router.include_router(personal_router, prefix="/personal", tags=["Personal"])
+router.include_router(talk_router, prefix="/talk", tags=["Talk"])
+router.include_router(channel_router, prefix="/channel", tags=["Channel"])
